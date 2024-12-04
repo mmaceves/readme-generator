f0b7273043ab00efeb52c043e46f3c4aa246bab7
Ace <matt.aceves@yahoo.com>
2024-12-03 23:08:44 -0700

    added files

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown';
+// TODO: Create an array of questions for user input
+const questions = [];
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: '',
+            name: ''
+        },
+    ])
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
