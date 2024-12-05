89c5dbdbc121e75b5b318c1345eb73b66e2e96fd
Ace <matt.aceves@yahoo.com>
2024-12-04 23:18:58 -0700

    fixed readme-sample

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((data) => {
-        writeToFile('README.md', generateMarkdown(data));
+        writeToFile('README-sample.md', generateMarkdown(data));
     });
 }
 
